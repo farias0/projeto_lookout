8f35a4c1ae626516bce61a02ce736569ac248922
Junior Farias <fariasjunior96@gmail.com>
2024-09-22 13:35:54 -0300

    Modified Inventory dimensions at runtime

--- ui/inventory/Inventory.cs
+++ ui/inventory/Inventory.cs
@@ -4,33 +4,47 @@ using projeto_lookout.libs;
 using System.Collections.Generic;
 using System.Linq;
 
-public class InventoryCell
-{
-	public ItemCell Cell { get; set; }
-	public InventoryItem HeldItem { get; set; }
-
-}
 
 public partial class Inventory : Control
 {
 	[Export]
-	public InventoryItem[] Items { 
-		get => GetItems();
-		set => SetItems(value); 
+	public int Columns { 
+		get => _columns; 
+		set
+		{
+			_columns = value;
+			UpdateGridSize();
+		}
+	}
+	[Export]
+	public int Rows
+	{
+		get => _rows;
+		set
+		{
+			_rows = value;
+			UpdateGridSize();
+		}
+	}
+	[Export]
+	public InventoryItem[] HeldItems // Sadly, modifying this list via inspector at runtime doesnt work. Something to do with serialization of Inventoryitems. TODO fix this
+	{
+		get => GetHeldItems();
+		set => SetHeldItems(value);
 	}
 
 
 	private static readonly PackedScene CellScene = (PackedScene)GD.Load("res://ui/inventory/items/cell/item_cell.tscn");
-	private const int Rows = 5;
-	private const int Columns = 7;
 
 
+	private int _columns = 7;
+	private int _rows = 5;
+	private ItemCell[] _cells = Array.Empty<ItemCell>();
 	private ColorRect _panel;
 	private Control _grid;
-	private readonly List<InventoryCell> _cells = new();
 
 
-	// Workaround for the setting the cells after they're created
+	// Workaround for only adding the items to the grid after the cells are created
 	private InventoryItem[] _delayedInitializationItems = null;
 
 
@@ -65,7 +79,7 @@ public partial class Inventory : Control
 
 		if (_delayedInitializationItems != null)
 		{
-			SetItems(_delayedInitializationItems);
+			SetHeldItems(_delayedInitializationItems);
 			_delayedInitializationItems = null;
 		}
 
@@ -91,82 +105,23 @@ public partial class Inventory : Control
 		else Enable();
 	}
 
-	/// <summary>
-	/// Attempts to drag an item from the inventory based on its new
-	/// position, and snaps it to the new cells if possible.
-	/// </summary>
-	/// <param name="item">Item that's been dragged</param>
-	/// <returns>If the item was successfully dragged</returns>
-	public bool AttemptItemDrag(InventoryItem item)
+	private void UpdateGridSize()
 	{
-		// TODO should check if item is in the inventory, not in the list
-		// Checks if it's in list
-		bool inList = false;
-		foreach (var i in Items) if (i == item) inList = true;
-		if (!inList) throw new InvalidOperationException("Item not in inventory.");
+		InventoryItem[] heldItems = GetHeldItems();
 
-
-		// Looks for the attempted slots
-		bool foundSlot = false;
-		Vector2 offset = Vector2.Zero;
-		foreach (var itemCell in item.Cells)
+		if (IsNodeReady())
 		{
-			foreach (var cell in _cells)
-			{
-				var dist = itemCell.GetGlobalPosition().DistanceTo(cell.Cell.GetGlobalPosition());
-				if (dist <= cell.Cell.Size.X / 2)
-				{
-					foundSlot = true;
-					offset = cell.Cell.GetGlobalPosition() - itemCell.GetGlobalPosition();
-					break;
-				}
-				if (foundSlot) break;
-			}
-		}
-		if (!foundSlot) return false;
-
-
-		var desiredCells = new List<InventoryCell>();
-
-		// Checks if every cell has the corresponding slot available
-		foreach (var itemCell in item.Cells)
-		{
-			bool found = false;
-			foreach (var cell in _cells)
-			{
-				var dist = itemCell.GetGlobalPosition().DistanceTo(cell.Cell.GetGlobalPosition());
-				if (dist <= cell.Cell.Size.X / 2 && (cell.HeldItem == null || cell.HeldItem == item))
-				{
-					found = true;
-					desiredCells.Add(cell);
-				}
-			}
-			if (!found) return false;
+			DestroyCells();
+			CreateCells();
 		}
 
-		// Frees previously occupied cells
-		foreach (var cell in _cells)
-		{
-			if (cell.HeldItem == item)
-			{
-				cell.HeldItem = null;
-			}
-		}
-
-		// Mark cells as occupied
-		foreach (var cell in desiredCells)
-		{
-			cell.HeldItem = item;
-		}
-
-		// Snaps to the new position
-		item.Position += offset;
-
-		return true;
+		SetHeldItems(heldItems);
 	}
 
 	private void CreateCells()
 	{
+		_cells = new ItemCell[Columns * Rows];
+
 		for (int i = 0; i < Rows; i++)
 		{
 			for (int j = 0; j < Columns; j++)
@@ -175,24 +130,29 @@ public partial class Inventory : Control
 				cell.Type = CellType.InventoryCell;
 				cell.Position = new Vector2(j * cell.Size.X, i * cell.Size.Y);
 				_grid.AddChild(cell);
-				_cells.Add(new InventoryCell()
-				{
-					Cell = cell,
-					HeldItem = null
-				});
+				_cells[(i * Columns) + j] = cell;
 			}
 		}
 	}
 
-	private InventoryItem[] GetItems()
+	private void DestroyCells()
+	{
+		foreach (var cell in _cells)
+		{
+			cell.QueueFree();
+		}
+		_cells = Array.Empty<ItemCell>();
+	}
+
+	private InventoryItem[] GetHeldItems()
 	{
 		if (_delayedInitializationItems != null)
 			return _delayedInitializationItems;
 		else
-			return _cells.Select(cell => cell.HeldItem).ToArray();
+			return _cells.Select(cell => cell.Item).Distinct().Where(item => item != null).ToArray();
 	}
 
-	private void SetItems(InventoryItem[] items)
+	private void SetHeldItems(InventoryItem[] items)
 	{
 		if (!IsNodeReady())
 		{
@@ -208,11 +168,11 @@ public partial class Inventory : Control
 			// Find an emtpy cell
 			foreach (var cell in _cells)
 			{
-				if (cell.HeldItem != null) continue;
+				if (cell.Item != null) continue;
 
 				// Attempt drag
 				var pos = item.GlobalPosition;
-				item.Position = cell.Cell.GetGlobalPosition();
+				item.Position = cell.GetGlobalPosition();
 				
 				if (AttemptItemDrag(item)) // TODO AttemptItemDrag is for mouse! Fix this hack
 				{
@@ -225,5 +185,77 @@ public partial class Inventory : Control
 			if (!success)
 				throw new InvalidOperationException($"Could not add item {item.Name} to the inventory.");
 		}
+	}
+
+	/// <summary>
+	/// Attempts to drag an item from the inventory based on its new
+	/// position, and snaps it to the grid if successful.
+	/// </summary>
+	/// <param name="item">Item that's been dragged</param>
+	/// <returns>If the item was successfully dragged</returns>
+	public bool AttemptItemDrag(InventoryItem item)
+	{
+
+		// Looks for the attempted slots
+		bool foundSlot = false;
+		Vector2 offset = Vector2.Zero;
+		foreach (var itemCell in item.Cells)
+		{
+			foreach (var gridCell in _cells)
+			{
+				var dist = itemCell.GetGlobalPosition().DistanceTo(gridCell.GetGlobalPosition());
+				if (dist <= gridCell.Size.X / 2)
+				{
+					foundSlot = true;
+					offset = gridCell.GetGlobalPosition() - itemCell.GetGlobalPosition();
+					break;
+				}
+				if (foundSlot) break;
+			}
+		}
+		if (!foundSlot) return false;
+
+
+		var desiredCells = new List<ItemCell>();
+
+		// Checks if every cell has the corresponding slot available
+		foreach (var itemCell in item.Cells)
+		{
+			bool found = false;
+			foreach (var cell in _cells)
+			{
+				var dist = itemCell.GetGlobalPosition().DistanceTo(cell.GetGlobalPosition());
+				if (dist <= cell.Size.X / 2 && (cell.Item == null || cell.Item == item))
+				{
+					found = true;
+					desiredCells.Add(cell);
+				}
+			}
+			if (!found) return false;
+		}
+
+
+		//		Move is successfull!!
+
+
+		// Frees previously occupied cells
+		foreach (var cell in _cells)
+		{
+			if (cell.Item == item)
+			{
+				cell.Item = null;
+			}
+		}
+
+		// Marks new cells as occupied
+		foreach (var cell in desiredCells)
+		{
+			cell.Item = item;
+		}
+
+		// Snaps to the new position
+		item.Position += offset;
+
+		return true;
 	}
 }
