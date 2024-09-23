071610d8c2fca244de8d95d0c1ef951f62f27a0f
Junior Farias <fariasjunior96@gmail.com>
2024-09-23 16:24:35 -0300

    Added Line2D and Square to the Draw lib

--- libs/Draw.cs
+++ libs/Draw.cs
@@ -41,5 +41,44 @@ namespace projeto_lookout.libs
 
 			return lineInstance;
 		}
+
+		/// <summary>
+		/// Draws a line in 2D space.
+		/// </summary>
+		/// <returns>The line instance.</returns>
+		public static Line2D Line2D(Node parent, Vector2 pointA, Vector2 pointB, Color color, float width = 1f)
+		{
+			Line2D line = new()
+			{
+				Points = new Vector2[] { pointA, pointB },
+				Width = width,
+				DefaultColor = color
+			};
+
+			parent.AddChild(line);
+
+			return line;
+		}
+	}
+
+	public class Square
+	{
+		public readonly Line2D[] Lines = new Line2D[4];
+
+		public void DrawSquare(Node parent, Rect2 points)
+		{
+			Lines[0] = Draw.Line2D(parent, points.Position, new Vector2(points.Position.X + points.Size.X, points.Position.Y), Colors.White);
+			Lines[1] = Draw.Line2D(parent, new Vector2(points.Position.X + points.Size.X, points.Position.Y), points.End, Colors.White);
+			Lines[2] = Draw.Line2D(parent, points.End, new Vector2(points.Position.X, points.Position.Y + points.Size.Y), Colors.White);
+			Lines[3] = Draw.Line2D(parent, new Vector2(points.Position.X, points.Position.Y + points.Size.Y), points.Position, Colors.White);
+		}
+
+		public void QueueFree()
+		{
+			foreach (var line in Lines)
+			{
+				line?.QueueFree();
+			}
+		}
 	}
 }
